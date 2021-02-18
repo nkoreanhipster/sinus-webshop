2b2070d8c1486caeaf1b161406b65c182491dc86
jakob andersson <jakob.aah@gmail.com>
2021-02-18 16:41:34 +0100

    Added more distinct error handling for login-form. - Each specific individual field is now marked with an error message - todo; do the same thing for other forms

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="bg-white p-3 m-2">
-    <form action="">
-      <input
-        class="mb-2 reset input"
-        type="text"
-        v-model="email"
-        placeholder="email"
-      />
-      <input
-        class="mb-2 reset input"
-        type="password"
-        v-model="password"
-        placeholder="password"
-      />
-      <button class="btn-black" @click="Login">Login</button>
+    <form method="POST" novalidate>
+      <div class="input-icon mv-2">
+        <input class="input" type="email" v-model="email" placeholder="email" />
+        <span class="icon"><i class="gg-mail"></i></span>
+      </div>
+      <p class="failure" style="font-size: 1.4rem">{{ errors[0] || "" }}</p>
+
+      <div class="input-icon mv-2">
+        <input
+          class="input"
+          type="password"
+          v-model="password"
+          placeholder="password"
+        />
+        <span class="icon"><i class="gg-lock"></i></span>
+      </div>
+      <p class="failure" style="font-size: 1.4rem">{{ errors[1] || "" }}</p>
+
+      <button class="btn-black w-icon" @click="Login">
+        <span>Login</span>
+        <i class="gg-log-in"></i>
+      </button>
 
       <router-link to="/register">
         <button class="btn-mustard m-2">Register</button>
@@ -24,27 +32,31 @@
 </template>
 
 <script>
+let errorMessages = {
+  hasEmail: `Email required.`,
+  hasPassword: `Password required.`,
+  validEmail: `Valid email required e.g. example@mail.com`,
+};
+
 export default {
   data() {
     return {
+      errors: [],
       message: "",
       email: "",
       password: "",
     };
   },
   mounted() {
-    this.message = ""; // Reset on component moun
+    //this.message = ""; // Reset on component moun
   },
   methods: {
     async Login(event) {
       event.preventDefault();
-      event.preventDefault();
-      this.message = "";
-      // Check if email/password are empty
-      if (this.email === "" || this.password === "") {
-        this.message = "Fields cannot be empty";
-        return;
-      }
+
+      this.checkFormsErrors();
+
+      if (this.errors.length > 0) return;
 
       let payload = {
         email: this.email,
@@ -61,6 +73,32 @@ export default {
         }
       }
       this.message = res;
+    },
+
+    /**
+     *    Check if form is valid, mark bad input fields with red
+     *    Errors are logged in this.errors for view display
+     */
+    checkFormsErrors() {
+      this.errors = [];
+
+      let inputCheck = {
+        hasEmail: this.email.length > 0,
+        hasPassword: this.password.length > 0,
+        validEmail: this.isEmailValidForm(this.email),
+      };
+
+      console.log({ inputCheck });
+
+      Object.entries(inputCheck)
+        .filter(([key, val]) => val === false)
+        .map(([key, val]) => this.errors.push(errorMessages[key]));
+    },
+
+    isEmailValidForm(email) {
+      var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      console.log({ email }, re.test(email));
+      return re.test(email);
     },
   },
 };
---
+++ frontend/src/components/single_elements/InputWithIcon.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <label :for="name"></label>
+    <input :name="name" :type="type" :placeholder="placeholder" :value="value" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    type: String,
+    placeholder: String,
+    value: String,
+  },
+};
+</script>
